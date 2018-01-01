d7099be402e435910a3b724f5761751a060a45b4
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2018-01-01 23:49:53 +0000

    Update simplebutton.py

--- simplebutton.py
+++ simplebutton.py
@@ -3,10 +3,10 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
-    input_state = GPIO.input(15)
+    input_state = GPIO.input(24)
     if input_state == False:
         print('Button Pressed')
         time.sleep(0.2)
